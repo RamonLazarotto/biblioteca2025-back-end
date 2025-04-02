a38cb31f1e0e9acf622fdc94767c24e99c0fbac6
RamonLazarotto <96747716+RamonLazarotto@users.noreply.github.com>
2025-04-01 21:10:35 -0300

    Implementada a conexão com o banco de dados

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 import express from "express";
+import { Sequelize } from "sequelize"; //Usa as chaves pois dentro da biblioteca dentro dela existem vários métodos
+                                       //Dentro das chaves fica somente o método que vai usar
+
+const sequelize = new Sequelize('biblioteca2025', 'postgres', '1234', {
+  host: 'localhost',
+  port: 5432,
+  dialect: 'postgres',
+  define: {
+    timestamps: false,
+    freezeTableName: true
+  }
+});
+
+try {
+    await sequelize.authenticate();
+    console.log('Conexão com o Banco de Dados estabelecida com sucesso!.');
+  } catch (error) {
+    console.error('Erro ao conectar-se com o banco de dados: ', error);
+  }
 
 const app = express();
 app.use(express.json());

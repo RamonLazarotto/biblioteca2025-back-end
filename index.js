fa7d84f5ba2d3bb95ed8441d9546b51d9cc8b14c
RamonLazarotto <96747716+RamonLazarotto@users.noreply.github.com>
2025-04-01 21:31:46 -0300

    mapeamento da model editora

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { Sequelize } from "sequelize"; //Usa as chaves pois dentro da biblioteca dentro dela existem vários métodos
-                                       //Dentro das chaves fica somente o método que vai usar
+import { Sequelize, DataTypes } from "sequelize"; //Usa as chaves pois dentro da biblioteca dentro dela existem vários métodos
+                                                  //Dentro das chaves fica somente o método que vai usar
 
+
+//Configuração com o Banco
 const sequelize = new Sequelize('biblioteca2025', 'postgres', '1234', {
   host: 'localhost',
   port: 5432,
@@ -12,11 +14,37 @@ const sequelize = new Sequelize('biblioteca2025', 'postgres', '1234', {
   }
 });
 
+//Mapeamento do modelo Editora
+const Editora = sequelize.define(
+    'editora',
+    {
+      // atributos do modelo são definidos aqui
+      ideditora: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      nomeeditora: {
+        type: DataTypes.STRING(60),
+        allowNull: false
+      },
+      cnpj: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+      },
+      endereco: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      }
+    }
+  );
+
 try {
     await sequelize.authenticate();
-    console.log('Conexão com o Banco de Dados estabelecida com sucesso!.');
+    console.log('Conexão com o Banco de Dados estabelecida com sucesso.');
   } catch (error) {
-    console.error('Erro ao conectar-se com o banco de dados: ', error);
+    console.error('Erro ao conectar-se com o banco de dados:', error);
   }
 
 const app = express();

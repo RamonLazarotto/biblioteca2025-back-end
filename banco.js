988146f91b9f0a738df7d79602bc539cb71a12db
RamonLazarotto <96747716+RamonLazarotto@users.noreply.github.com>
2025-04-08 20:37:15 -0300

    Adicionado organização no código

---
+++ banco.js
@@ -0,0 +1,16 @@
+import { Sequelize, DataTypes, where } from "sequelize"; //Usa as chaves pois dentro da biblioteca dentro dela existem vários métodos
+                                                         //Dentro das chaves fica somente o método que vai usar
+
+
+//Configuração com o Banco
+const sequelize = new Sequelize('biblioteca2025', 'postgres', '1234', {
+  host: 'localhost',
+  port: 5432,
+  dialect: 'postgres',
+  define: {
+    timestamps: false,
+    freezeTableName: true
+  }
+});
+
+export default sequelize;
--- index.js
+++ index.js
@@ -1,47 +1,9 @@
 import express from "express";
-import { Sequelize, DataTypes, where } from "sequelize"; //Usa as chaves pois dentro da biblioteca dentro dela existem vários métodos
-                                                  //Dentro das chaves fica somente o método que vai usar
-
-
-//Configuração com o Banco
-const sequelize = new Sequelize('biblioteca2025', 'postgres', '1234', {
-  host: 'localhost',
-  port: 5432,
-  dialect: 'postgres',
-  define: {
-    timestamps: false,
-    freezeTableName: true
-  }
-});
-
-//Mapeamento do modelo Editora
-const Editora = sequelize.define(
-    'editora',
-    {
-      // atributos do modelo são definidos aqui
-      ideditora: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      nomeeditora: {
-        type: DataTypes.STRING(60),
-        allowNull: false
-      },
-      cnpj: {
-        type: DataTypes.STRING(20),
-        allowNull: false
-      },
-      endereco: {
-        type: DataTypes.TEXT,
-        allowNull: true
-      }
-    }
-  );
+import banco from "./banco.js";
+import editora from "./controller/EditoraController.js";
 
 try {
-    await sequelize.authenticate();
+    await banco.authenticate();
     console.log('Conexão com o Banco de Dados estabelecida com sucesso.');
   } catch (error) {
     console.error('Erro ao conectar-se com o banco de dados:', error);
@@ -50,50 +12,16 @@ try {
 const app = express();
 app.use(express.json());
 
-app.get('/teste', (req, res) => {
-    res.send("Teste ok")
-})
-
 //rotas crud da tabela editora
-app.get('/editora', async (req, res) => {
-    const respostaBanco = await Editora.findAll();
-    res.json(respostaBanco);
-});
+app.get('/editora', editora.listar);
 
-app.get('/editora/:id', async (req, res) => {
-    const id = req.params.id;
-    const respostaBanco = await Editora.findByPk(id);
-    res.json(respostaBanco);
-});
+app.get('/editora/:id', editora.selecionar);
 
-app.post('/editora', async (req, res) => {
-//    const nomeeditora = req.body.nomeeditora;
-//    const cnpj = req.body.cnpj
-//    const endereco = req.body.endereco
+app.post('/editora', editora.inserir);
 
-    const respostaBanco = await Editora.create(req.body);
-    res.json(respostaBanco);
-});
+app.put('/editora/:id', editora.alterar);
 
-app.put('/editora/:id', async (req, res) => {
-        const nomeeditora = req.body.nomeeditora;
-        const cnpj = req.body.cnpj
-        const endereco = req.body.endereco
+app.delete('/editora/:id', editora.excluir)
 
-        const ideditora = req.params.ideditora
-    
-        const respostaBanco = await Editora.update(
-            {nomeeditora, cnpj, endereco},
-            {where: {ideditora} });
-        res.json(respostaBanco);
-    });
-
-app.delete('/editora/:id', async (req, res) => {
-          const ideditora = req.params.ideditora
-
-          const respostaBanco = await Editora.destroy(
-            {where: {ideditora} });
-          res.json(respostaBanco);
-})
-
+//Mensagem de que o servidor rodando
 app.listen(3000, () => {console.log("Servidor Rodando.") });
